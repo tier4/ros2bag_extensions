71a7a13dd972d2d59d5f471b4a9aa5145d9282ce
h-ohta <hiroki.ota@tier4.jp>
2023-03-10 16:08:49 +0900

    feat(slice): add duration option

--- ros2bag_extensions/ros2bag_extensions/verb/slice.py
+++ ros2bag_extensions/ros2bag_extensions/verb/slice.py
@@ -18,13 +18,15 @@ import os
 from ros2bag.api import check_path_exists
 from ros2bag.verb import VerbExtension
 from rosbag2_py import *
+from rosbag2_py_wrapper import SequentialWriterWrapper
 
 from . import create_reader, get_default_converter_options, get_default_storage_options
 
 
 class SliceVerb(VerbExtension):
+
     ''' Save the specified range of data as a bag file by specifying the start time and end time. '''
-    def _bag2slice(self, input_bag_dir: str, output_bag_dir: str, start_time: datetime.datetime, end_time: datetime.datetime) -> None:
+    def _bag2slice_with_start_end_time(self, input_bag_dir: str, output_bag_dir: str, start_time: datetime.datetime, end_time: datetime.datetime) -> None:
         # Check timestamp
         metadata = Info().read_metadata(input_bag_dir, "sqlite3")
 
@@ -54,6 +56,33 @@ class SliceVerb(VerbExtension):
             if start_time <= datetime_stamp <= end_time:
                 writer.write(topic_name, msg, stamp)
 
+    ''' Split and save bag files by specifying the duration. '''
+    def _bag2slice_with_duration(self, input_bag_dir: str, output_bag_dir: str, duration: float) -> None:
+        # Open writer
+        storage_options = get_default_storage_options(output_bag_dir)
+        converter_options = get_default_converter_options()
+        writer = SequentialWriterWrapper()
+        writer.open(storage_options, converter_options)
+
+        # Create topics
+        reader = create_reader(input_bag_dir)
+        for topic_type in reader.get_all_topics_and_types():
+            writer.create_topic(topic_type)
+
+        # Write
+        split_timestamp = None
+        while reader.has_next():
+            topic_name, msg, stamp = reader.read_next()
+            if split_timestamp is None:
+                split_timestamp = stamp
+            writer.write(topic_name, msg, stamp)
+            time_diff = (stamp - split_timestamp) / 1e9
+            if time_diff > duration:
+                writer.split_bagfile()
+                split_timestamp = stamp
+
+
+
 
     def add_arguments(self, parser, cli_name):
         parser.add_argument(
@@ -61,16 +90,23 @@ class SliceVerb(VerbExtension):
         parser.add_argument(
             "-o", "--output", required=True, help="Output directory")
         parser.add_argument(
-            "-s", "--start-time", default=0.0, type=float, help="Start time in nanoseconds")
+            "-s", "--start-time", type=float, help="Start time in nanoseconds")
         parser.add_argument(
-            "-e", "--end-time", default=4102412400, type=float, help="End time in nanoseconds") # 2100/01/01 00:00:00
-
+            "-e", "--end-time", type=float, help="End time in nanoseconds") # 2100/01/01 00:00:00
+        parser.add_argument(
+            "-d", "--duration", type=float, help="duration second for slice")
 
     def main(self, *, args):
         if os.path.isdir(args.output):
             raise FileExistsError("Output folder '{}' already exists.".format(args.output))
 
-        dt_start_time = datetime.datetime.fromtimestamp(args.start_time)
-        dt_end_time = datetime.datetime.fromtimestamp(args.end_time)
+        # start_time and end_time exists
+        if args.start_time is not None and args.end_time is not None:
+            dt_start_time = datetime.datetime.fromtimestamp(args.start_time)
+            dt_end_time = datetime.datetime.fromtimestamp(args.end_time)
 
-        self._bag2slice(args.bag_directory, args.output, dt_start_time, dt_end_time)
+            self._bag2slice_with_start_end_time(args.bag_directory, args.output, dt_start_time, dt_end_time)
+        elif args.duration is not None:
+            self._bag2slice_with_duration(args.bag_directory, args.output, args.duration)
+        else:
+            print("please specify start and end time or duration")
