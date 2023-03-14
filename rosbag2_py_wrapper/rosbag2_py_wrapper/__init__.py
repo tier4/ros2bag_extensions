3104a6d1ba9417fd78fed19837520c0bd10021cb
Hiroki OTA <hiroki.ota@tier4.jp>
2023-03-14 13:49:02 +0900

    feat(slice): add duration option (#20)
    * feat(slice): add duration option
    
    * Update slice.py
    
    * Update package.xml
    
    * fix: build error and delete unnecessary

---
+++ rosbag2_py_wrapper/rosbag2_py_wrapper/__init__.py
@@ -0,0 +1,28 @@
+# Copyright 2023 TierIV
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from rpyutils import add_dll_directories_from_env
+
+# Since Python 3.8, on Windows we should ensure DLL directories are explicitly added
+# to the search path.
+# See https://docs.python.org/3/whatsnew/3.8.html#bpo-36085-whatsnew
+with add_dll_directories_from_env('PATH'):
+    from rosbag2_py_wrapper._writer import (
+        SequentialWriterWrapper,
+    )
+
+
+__all__ = [
+    'SequentialWriterWrapper',
+]
---
+++ rosbag2_py_wrapper/src/rosbag2_py_wrapper/_writer.cpp
@@ -0,0 +1,64 @@
+// Copyright 2023 TierIV
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <unordered_set>
+
+#include "rosbag2_cpp/writers/sequential_writer.hpp"
+#include "rosbag2_storage/ros_helper.hpp"
+
+#include "./pybind11.hpp"
+
+namespace rosbag2_py_wrapper
+{
+
+class SequentialWriterWrapper : public rosbag2_cpp::writers::SequentialWriter
+{
+public:
+  void split_bagfile_wrapper() {
+    this->split_bagfile();
+  }
+
+  /// Write a serialized message to a bag file
+  void write(
+    const std::string & topic_name, const std::string & message,
+    const rcutils_time_point_value_t & time_stamp)
+  {
+    auto bag_message =
+      std::make_shared<rosbag2_storage::SerializedBagMessage>();
+
+    bag_message->topic_name = topic_name;
+    bag_message->serialized_data =
+      rosbag2_storage::make_serialized_message(message.c_str(), message.length());
+    bag_message->time_stamp = time_stamp;
+
+    rosbag2_cpp::writers::SequentialWriter::write(bag_message);
+  }
+};
+}  // namespace rosbag2_py_wrapper
+
+PYBIND11_MODULE(_writer, m) {
+  m.doc() = "Python wrapper of the SequentialWriter API";
+
+  pybind11::class_<rosbag2_py_wrapper::SequentialWriterWrapper>(
+    m, "SequentialWriterWrapper")
+  .def(pybind11::init())
+  .def("open", &rosbag2_py_wrapper::SequentialWriterWrapper::open)
+  .def("write", &rosbag2_py_wrapper::SequentialWriterWrapper::write)
+  .def("remove_topic", &rosbag2_py_wrapper::SequentialWriterWrapper::remove_topic)
+  .def("create_topic", &rosbag2_py_wrapper::SequentialWriterWrapper::create_topic)
+  .def("split_bagfile", &rosbag2_py_wrapper::SequentialWriterWrapper::split_bagfile_wrapper);
+}
