d3d8abc4a352f3baf6d38953b2b63b5c794230b7
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-06-24 14:40:29 +0900

    feat: support latched topics (#26)
    * feat: support latched-topic for slice function
    
    * fix: add argument
    
    * fix: pop kept_topics

--- ros2bag_extensions/ros2bag_extensions/verb/slice.py
+++ ros2bag_extensions/ros2bag_extensions/verb/slice.py
@@ -25,7 +25,7 @@ from . import create_reader, get_default_converter_options, get_default_storage_
 
 class SliceVerb(VerbExtension):
     ''' Save the specified range of data as a bag file by specifying the start time and end time. '''
-    def _bag2slice_with_start_end_time(self, input_bag_dir: str, output_bag_dir: str, start_time: datetime.datetime, end_time: datetime.datetime) -> None:
+    def _bag2slice_with_start_end_time(self, input_bag_dir: str, output_bag_dir: str, start_time: datetime.datetime, end_time: datetime.datetime, latched_topic: list[str]) -> None:
         # Check timestamp
         metadata = Info().read_metadata(input_bag_dir, "sqlite3")
 
@@ -48,12 +48,20 @@ class SliceVerb(VerbExtension):
         for topic_type in reader.get_all_topics_and_types():
             writer.create_topic(topic_type)
 
+        kept_topics = []
+
         # Write
         while reader.has_next():
             topic_name, msg, stamp = reader.read_next()
             datetime_stamp = datetime.datetime.fromtimestamp(stamp / 1e9)
             if start_time <= datetime_stamp <= end_time:
                 writer.write(topic_name, msg, stamp)
+                while kept_topics:
+                    kept_topic = kept_topics.pop(0)
+                    writer.write(kept_topic[0], kept_topic[1], stamp)
+
+            elif topic_name in latched_topic:
+                kept_topics.append((topic_name, msg))
 
     ''' Split and save bag files by specifying the duration. '''
     def _bag2slice_with_duration(self, input_bag_dir: str, output_bag_dir: str, duration: float) -> None:
@@ -91,6 +99,9 @@ class SliceVerb(VerbExtension):
             "-e", "--end-time", default=4102412400, type=float, help="End time in nanoseconds")  # 2100/01/01 00:00:00
         parser.add_argument(
             "-d", "--duration", type=float, help="duration second for slice")
+        parser.add_argument(
+            "-l", "--latched-topics", nargs="*", type=str, help="list of latched topics", default=[]
+        )
 
     def main(self, *, args):
         if os.path.isdir(args.output):
@@ -102,4 +113,4 @@ class SliceVerb(VerbExtension):
         else:  # start and end mode
             dt_start_time = datetime.datetime.fromtimestamp(args.start_time)
             dt_end_time = datetime.datetime.fromtimestamp(args.end_time)
-            self._bag2slice_with_start_end_time(args.bag_directory, args.output, dt_start_time, dt_end_time)
+            self._bag2slice_with_start_end_time(args.bag_directory, args.output, dt_start_time, dt_end_time, args.latched_topics)
